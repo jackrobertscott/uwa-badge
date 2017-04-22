14d91ca93e54be166d91570d17416883a2a14022
Jack Scott <mast.j.scott@gmail.com>
2017-04-22 17:06:08 +0800

    generate some starter components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,16 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { ChatBoxComponent } from './chat/chat-box/chat-box.component';
+import { ChatListComponent } from './chat/chat-list/chat-list.component';
+import { ChatListItemComponent } from './chat/chat-list-item/chat-list-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChatBoxComponent,
+    ChatListComponent,
+    ChatListItemComponent
   ],
   imports: [
     BrowserModule,
